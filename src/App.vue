fb5b834bd69775ad08d759250ef7e058925f70fa
Lütfi Kotan <lutfiktoann@gmail.com>
2022-08-31 18:46:03 +0300

    version 0.03

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,17 @@ import { ref } from "vue";
 import Tabs from "./components/Tabs.vue";
 import Tab from "./components/Tab.vue";
 
-const tabs = ref(["Showcased Accessories", "Interface Modules", "Network Access Points", "Network Routers"]);
+const tabs = ref([
+  "Showcased Accessories",
+  "Interface Modules",
+  "Network Access Points",
+  "Network Routers",
+]);
+const version = APP_VERSION;
 </script>
 
 <template>
+{{version}}
   <Tabs>
     <Tab v-for="tab in tabs" :key="tab" :title="tab">Hello {{ tab }}</Tab>
   </Tabs>
